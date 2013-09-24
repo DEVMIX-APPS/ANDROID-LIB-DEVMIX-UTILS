d17bf84a16b00eaeb95937ec70c59d8efa3b1596
ALAN ECHER <alan.echer@gmail.com>
2013-09-24 15:19:01 -0300

    Adding DataBaseManager backup and restore

---
+++ DEVMIX UTILS/src/com/devmix/libs/utils/dao/DataBaseManager.java
@@ -0,0 +1,45 @@
+package com.devmix.libs.utils.dao;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.content.Context;
+import android.os.Environment;
+
+import com.devmix.libs.utils.files.FileManager;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+
+public class DataBaseManager {
+	public static void restore(Context context,String backupDir,String dbName,OrmLiteSqliteOpenHelper dbHelper) throws IOException {
+	    if (Environment.getExternalStorageDirectory().canWrite()) {
+	        File fileBackup = new File(backupDir, dbName);
+	        if (!fileBackup.exists()) {
+	            throw new IOException("Arquivo não encontrado!");
+	        }
+	        dbHelper.close();
+			dbHelper = null;
+
+			File file = new File(context.getDatabasePath(dbName).toString());
+			if(!FileManager.copy(fileBackup.getPath().toString(), file.getPath().toString())){
+				throw new IOException("Não foi possível realizar a restauração");
+			}
+	    } else {
+	        throw new IOException("Sem permissão para escrita no SD Card");
+	    }
+	}
+	public static void backup(Context context,String backupDir,String dbName,String dbBackupName,OrmLiteSqliteOpenHelper dbHelper) throws IOException{
+		if (Environment.getExternalStorageDirectory().canWrite()) {
+			File fileBackup = new File(backupDir, dbBackupName);
+
+	        dbHelper.close();
+			dbHelper = null;
+
+			File file = new File(context.getDatabasePath(dbName).toString());
+			if(!FileManager.copy(file.getPath().toString(),fileBackup.getPath().toString())){
+				throw new IOException("Não foi possível realizar o backup");
+			}
+		} else {
+	        throw new IOException("Sem permissão para escrita no SD Card");
+	    }
+	}
+}
