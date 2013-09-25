a18fc94c11a7a6250d0c39161b1630d68faaf926
ALAN ECHER <alan.echer@gmail.com>
2013-09-25 08:50:09 -0300

    news DataBaseActions, ReflectionUtils

--- DEVMIX UTILS/src/com/devmix/libs/utils/dao/DataBaseActions.java
+++ DEVMIX UTILS/src/com/devmix/libs/utils/dao/DataBaseActions.java
@@ -7,9 +7,15 @@ public interface DataBaseActions {
 
     public long save(Object object);
 
-    public Object saveIfNotExist(Object object);
+    public Object saveReturn(Object object);
+
+    public long saveIfNotExist(Object object);
+
+    public Object saveIfNotExistReturn(Object object);
 
     public long update(Object object);
+
+    public Object updateReturn(Object object);
 
     public Object load(int id);
 
